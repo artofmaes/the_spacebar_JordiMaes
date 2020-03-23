8f6a5bae6e97274d43c2b9d6365430e83609f2a0
artofmaes <jordi19@live.be>
2020-03-23 19:57:03 +0100

    Twig verder uitgewerkt

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -20,7 +21,17 @@ class ArticleController
     /**
      * @Route("/news/{slug}")
      */
-    public function show($slug){
-        return new Response(sprintf('Future page to show later: %s',$slug));
+    public function show($slug)
+    {
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+        return $this->render('article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments
+        ]);
     }
 }
