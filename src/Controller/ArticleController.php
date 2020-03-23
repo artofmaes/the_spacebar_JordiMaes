cf6ead1ec7bcc53f71775289d87815f3c8961582
artofmaes <jordi19@live.be>
2020-03-23 19:26:42 +0100

    making such good progress

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Controller;
+
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage(){
+        return new Response('OMG MY FIRST PAGE!');
+    }
+
+    /**
+     * @Route("/news/{slug}")
+     */
+    public function show($slug){
+        return new Response(sprintf('Future page to show later: %s',$slug));
+    }
+}
