7dd7a5a05b8a42cbad23ffe31111bbe74417ae04
artofmaes <jordi19@live.be>
2020-04-21 17:16:08 +0200

    Course 4 - Chapter 09 -12

--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -2,8 +2,10 @@
 
 namespace App\Entity;
 
+use App\Repository\ArticleRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
@@ -175,6 +177,17 @@ class Article
     {
         return $this->comments;
     }
+
+    /**
+     * @return Collection|Comment[]
+     */
+    public function getNonDeletedComments(): Collection
+    {
+        $criteria = ArticleRepository::createNonDeletedCriteria();
+
+        return $this->comments->matching($criteria);
+    }
+
 
     public function addComment(Comment $comment): self
     {
